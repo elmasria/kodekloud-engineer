ce6764a0ef802c44eb5f6922b33e74f3889c8ef2
Ahmad El Masri <ahmadjelmasri@gmail.com>
2023-04-28 13:13:00 +0200

    feat: linux timezones

---
+++ System Administrator/49_Linux_TimeZones_Setting/49_Linux_TimeZones_Setting.sh
@@ -0,0 +1,11 @@
+sshpass -p  '***' ssh -o StrictHostKeyChecking=no tony@172.16.238.10
+sshpass -p  '***' ssh -o StrictHostKeyChecking=no steve@172.16.238.11
+sshpass -p  '***' ssh -o StrictHostKeyChecking=no banner@172.16.238.12
+
+
+sudo su -
+
+timedatectl
+timedatectl -h
+
+timedatectl set-timezone Asia/Irkutsk
